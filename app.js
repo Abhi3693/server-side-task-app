df4e33f4fda4b549623b09eee7a2aad6b1323aa7
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-06-07 10:02:10 +0530

    Added server-side tasks application

---
+++ app.js
@@ -0,0 +1,38 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+var tasksRouter = require('./routes/tasks');
+var usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb://localhost/task', (err) => {
+  console.log(err ? err : 'Connected to database');
+});
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/tasks', tasksRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+module.exports = app;
